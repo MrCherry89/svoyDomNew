7805ec70fd9afb03c5030caadeb57c8559ad3fae
MrCherry89 <abgarnati@yandex.ru>
2024-08-19 14:37:34 +0400

    ok

--- js/script.js
+++ js/script.js
@@ -493,12 +493,8 @@ $(document).ready(function () {
     infinite: true,
     slidesToShow: 1,
     slidesToScroll: 1,
-    prevArrow: $(
-      ".main-slider-wrap .wrapper2 .olen-slider-navigation .slick-prev"
-    ),
-    nextArrow: $(
-      ".main-slider-wrap .wrapper2 .olen-slider-navigation .slick-next"
-    ),
+    prevArrow: $(".main-slider-wrap  .olen-slider-navigation .slick-prev"),
+    nextArrow: $(".main-slider-wrap  .olen-slider-navigation .slick-next"),
     speed: 700,
     cssEase: "ease-in-out",
   });
@@ -509,12 +505,8 @@ $(document).ready(function () {
     infinite: true,
     slidesToShow: 1,
     slidesToScroll: 1,
-    prevArrow: $(
-      ".main-slider-wrap2 .wrapper2 .olen-slider-navigation .slick-prev"
-    ),
-    nextArrow: $(
-      ".main-slider-wrap2 .wrapper2 .olen-slider-navigation .slick-next"
-    ),
+    prevArrow: $(".main-slider-wrap2  .olen-slider-navigation .slick-prev"),
+    nextArrow: $(".main-slider-wrap2  .olen-slider-navigation .slick-next"),
     speed: 700,
     cssEase: "ease-in-out",
   });
@@ -540,6 +532,18 @@ $(document).ready(function () {
         },
       },
     ],
+  });
+
+  $(".gakku-planing-slider").slick({
+    dots: false,
+    arrows: true,
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    prevArrow: $(".gakku-planing-slider-wrap .slider-navigation .slick-prev"),
+    nextArrow: $(".gakku-planing-slider-wrap .slider-navigation .slick-next"),
+    speed: 700,
+    cssEase: "ease-in-out",
   });
 
   var swiper = new Swiper(".swiper-container", {
@@ -727,6 +731,78 @@ $(document).ready(function () {
     });
   });
 
+  $(".tab2").on("click", function () {
+    var tabId = $(this).data("tab");
+
+    $(".tab2").removeClass("active");
+    $(this).addClass("active");
+
+    $(".tab-content2").removeClass("active");
+    $('.tab-content2[data-content="' + tabId + '"]').addClass("active");
+  });
+
+  // Устанавливаем активную первую вкладку по умолчанию
+  $(".tab2:first").addClass("active");
+  $(".tab-content2:first").addClass("active");
+
+  const tabButtons2 = document.querySelectorAll(".tab-button");
+  const tabPanes2 = document.querySelectorAll(".tab-pane");
+
+  tabButtons2.forEach((button) => {
+    button.addEventListener("click", () => {
+      const targetTab = button.getAttribute("data-tab");
+
+      tabButtons2.forEach((btn) => btn.classList.remove("active"));
+      tabPanes2.forEach((pane) => {
+        pane.classList.remove("active");
+        pane.classList.remove("fade-in");
+      });
+
+      button.classList.add("active");
+      document.getElementById(targetTab).classList.add("active", "fade-in");
+    });
+  });
+
+  $(".tab3").on("click", function () {
+    var tabId = $(this).data("tab");
+
+    $(".tab3").removeClass("active");
+    $(this).addClass("active");
+
+    $(".tab-content3").removeClass("active");
+    $('.tab-content3[data-content="' + tabId + '"]').addClass("active");
+  });
+
+  // Устанавливаем активную первую вкладку по умолчанию
+  $(".tab3:first").addClass("active");
+  $(".tab-content3:first").addClass("active");
+
+  const tabButtons3 = document.querySelectorAll(".tab-button");
+  const tabPanes3 = document.querySelectorAll(".tab-pane");
+
+  tabButtons2.forEach((button) => {
+    button.addEventListener("click", () => {
+      const targetTab = button.getAttribute("data-tab");
+
+      tabButtons3.forEach((btn) => btn.classList.remove("active"));
+      tabPanes3.forEach((pane) => {
+        pane.classList.remove("active");
+        pane.classList.remove("fade-in");
+      });
+
+      button.classList.add("active");
+      document.getElementById(targetTab).classList.add("active", "fade-in");
+    });
+  });
+
+  $(".gakku-drop-menu").click(function (e) {
+    e.stopPropagation();
+    $(this).toggleClass("is-active");
+    $(".gakku-mobile-menu").toggleClass("open");
+    $(".gakku-header").toggleClass("bg");
+    $("body, html").toggleClass("overflow");
+  });
+
   $(".layouts-slider").slick({
     slidesToShow: 3,
     slidesToScroll: 1,
@@ -746,6 +822,27 @@ $(document).ready(function () {
           centerMode: true,
           centerPadding: "40px",
           slidesToShow: 1,
+        },
+      },
+    ],
+  });
+
+  $(".gakku-banner-slider").slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    dots: true,
+    arrows: false,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    cssEase: "linear",
+    variableWidth: true,
+    focusOnSelect: true,
+    centerMode: true,
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          arrows: false,
         },
       },
     ],
